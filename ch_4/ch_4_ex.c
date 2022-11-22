cd704b66d2bd99006a6685271d7a08d1e9c34ca8
Laura Vail <vail.laura@gmail.com>
2022-11-22 18:35:28 -0500

    4-3 part two partially working

--- ch_4/ch_4_ex.c
+++ ch_4/ch_4_ex.c
@@ -296,24 +296,38 @@ void ungetch(int);
 
 // getop: get next operator or numeric operand
 int getop(char s[]) {
-	int i, c;
+	int i, c, next, sign;
 
-	while ((s[0] = c = getch()) == ' ' || c == '\t')
+	sign = 1;    // default sign of numbers to positive
+
+	while ((s[0] = c = getch()) == ' ' || c == '\t')    // skip whitespace, between chars
 		;
 	s[1] = '\0';
+
+	if (c == '-') { 		// check if '-' is subtraction operator, or means negative number
+		next = getch();
+		if (next == ' ' || next == '\t')	// return subtraction operator if whitespace after
+			return c;
+		else {
+			sign = -1;
+			ungetch(next); 
+		}
+	}
+
 	if (!isdigit(c) && c != '.')
-		return c; 		// not a number
+		return c; 		// not a number, so return it
+	
 	i = 0;
 	if (isdigit(c))    	// collect integer part
 		while (isdigit(s[++i] = c = getch()))
 			;
-	if (c == '.')
+	if (c == '.')		// get part after a decimal
 		while (isdigit(s[++i] = c = getch()))
 			;
 	s[i] = '\0';
 	if (c != EOF)
 		ungetch(c);
-	return NUMBER;
+	return NUMBER * sign;
 }
 
 
