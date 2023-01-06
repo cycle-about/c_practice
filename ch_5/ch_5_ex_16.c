dfb1ed2de43080245905b9eccac5ddc7f28f008e
Laura Vail <vail.laura@gmail.com>
2023-01-06 10:58:17 -0500

    Initial setup of 5-16

---
+++ ch_5/ch_5_ex_16.c
@@ -0,0 +1,289 @@
+/********************************************************************************
+5-16 Add the -d ("directory order") option, which makes comparisons only on letters, 
+numbers, and blanks. Make sure it works in conjunction with -f.
+
+Compare only chars when this is true:
+	
+	(isalnum(c) != 0 || c == ' ')
+
+Idea: Use the copy-of-modified-string setup from 5-15, and have the copy omit all
+chars not meeting that criteria, and then compare pointers to those copies
+
+Note: this means the 'regular' qsort ALSO needs the 'array copy' setup, so likely
+make into only a single qsort method for all cases again
+
+First version though: add this to the 'nocase' version of function
+
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+#define MAXLINES 5000 		// max number of lines to be sorted
+char *lineptr[MAXLINES]; 	// pointers to text lines
+
+int readlines(char *lineptr[], int nlines);
+int checkflag(char s[], int argc, char *argv[]);
+void writelines(char *lineptr[], int nlines);
+void qsort_ex(int reverse, void *lineptr[], int left, int right, int (*comp)(void *, void *));
+void qsort_nocase(int reverse, int nocase, int alnum, void *lineptr[], int left, int right, int (*comp)(void *, void *));
+int numcmp(char *, char *);
+
+// sort input lines: default is lexicographically, but flag '-n' means sort numerically
+// flag -r means sort in reverse order
+// gcc -o ch_5_ex_14.o ch_5_ex_14.c && ./ch_5_ex_14.o -n -r
+int main(int argc, char *argv[]) {
+	int nlines;  		// number of input lines read
+	int numeric = 0;	// default is lex sort, NOT numeric
+	int reverse = 0;	// default is ascending order, NOT reverse
+	int nocase = 0;   	// default lex sorting differentiates capitalization
+
+	numeric = checkflag("-n", argc, argv);
+	reverse = checkflag("-r", argc, argv);	// descending order
+	nocase = checkflag("-f", argc, argv);  	// ignores capitalization
+	alnum = checkflag("-d", argc, argv); 	// compares only letters, numbers, and ' '
+
+	if ((nlines = readlines(lineptr, MAXLINES)) >= 0) {
+		// in qsort call: 'strcmp' and 'numcmp' are addresses of functions
+		// 'numcmp' defined below, 'strcmp' is in 'string' library
+		
+		// MESSY FOR NOW
+		if (!numeric && (nocase || alnum)) {  // function version for lex with nocase or alnum
+			printf("\nUsing qsort_nocase\n");
+			qsort_nocase(reverse, nocase, alnum, (void **) lineptr, 0, nlines-1, (int (*)(void*,void*))(numeric ? numcmp : strcmp));
+		} else {  // function for numeric or case sensitive
+			printf("\nUsing original qsort\n");
+			qsort_ex(reverse, (void **) lineptr, 0, nlines-1, (int (*)(void*,void*))(numeric ? numcmp : strcmp));
+		}
+		writelines(lineptr, nlines);
+		return 0;
+	} else {
+		printf("input too big to sort\n");
+		return 1;
+	}
+}
+
+// returns 1 if provided string is in args array
+int checkflag(char s[], int argc, char *argv[]) {
+	for (int i = 1; i < argc; i++) {
+		if (strcmp(argv[i], s) == 0) {
+			return 1;
+		}
+	}
+	return 0;
+}
+
+// sort v[left]...v[right] into increasing order
+// int (*comp)(void *, void *)  -> 'comp' is pointer to a function with two void * args, returns int
+
+// comp 						-> pointer to a function
+// *comp 						-> the function
+// (*comp)(v[i], v[left]) 		-> call to the function made by qsort
+void qsort_ex(int reverse, void *v[], int left, int right, int (*comp)(void *, void *)) {
+	int i, last, compared;
+	void swap(void *v[], int, int);
+
+	if (left >= right) 		// do nothing if array contains fewer than 2 elements
+		return;
+	
+	swap(v, left, (left+right)/2);  // make a new 'pivot': swap index 0 and midpoint
+	last = left;
+	printf("\nleft: %s\n", (char *) v[left]);
+	
+	for (i = left+1; i <= right; i++) {
+		printf("i: %s\n", (char *) v[i]);
+		compared = (*comp)(v[i], v[left]);
+		if (!reverse) { 	// ascending order
+			if (compared < 0) {
+				swap(v, ++last, i);
+			}
+		} else { 			// reverse order
+			if (compared > 0) {
+				swap(v, ++last, i);
+			}
+		}
+	}
+	swap(v, left, last);
+	qsort_ex(reverse, v, left, last-1, comp);
+	qsort_ex(reverse, v, last+1, right, comp);
+}
+
+#include <stdlib.h>
+
+void intolower(const char *str, char dest[]);
+
+// version of qsort for lexicographic sorting when ignoring case, or 'directory order'
+void qsort_nocase(int reverse, int nocase, int alnum, void *v[], int left, int right, int (*comp)(void *, void *)) {
+	int i, last, compared;
+	void swap(void *v[], int, int);
+	char leftlower[100];  	// array to store processed version of string
+	char ilower[100];
+	char *leftptr;  		// pointer to the processed arrays to use in compare function
+	char *iptr;
+
+	if (left >= right) 		// do nothing if array contains fewer than 2 elements
+		return;
+	
+	swap(v, left, (left+right)/2);  // make middle element the pivot, and move it into leftmost position
+	last = left;
+	
+	// printf("left orig: %s\n", (char *) v[left]);
+	
+	// TODO handle that either or both of these might run
+	if (nocase) 
+		intolower((char *) v[left], leftlower);  // put lower-case version of string at 'left' into 'leftlower'
+	if (alnum)
+
+	// printf("left lower arr: %s\n", leftlower);
+	leftptr = leftlower;
+	// printf("left lower ptr: %s\n", leftptr);
+	
+	for (i = left+1; i <= right; i++) {
+		
+		// printf("i: %s\n", (char *) v[i]);
+		intolower((char *) v[i], ilower);  // put lower-case version of string at i into 'ilower'
+		// printf("ilower arr: %s\n", ilower);
+		iptr = ilower;
+		// printf("ilower ptr: %s\n", iptr);
+
+		compared = (*comp)(iptr, leftptr);  // compare the lower-case string versions
+		
+		// swap locations of the original with-capital strings as needed
+		if (!reverse) { 	// ascending order
+			if (compared < 0) {
+				swap(v, ++last, i);
+			}
+		} else { 			// reverse order
+			if (compared > 0) {
+				swap(v, ++last, i);
+			}
+		}
+		
+	}
+	swap(v, left, last);  // move pivot to its correct position
+	qsort_nocase(reverse, v, left, last-1, comp);
+	qsort_nocase(reverse, v, last+1, right, comp);
+}
+
+#include <ctype.h>
+
+// put only letters, numbers, and ' ' into array for comparison
+void intoalnum(const char *str, char dest[]) {
+	int i, j, c;
+	char *strlow;
+	for (i = 0, j = 0; i < strlen(str); i++) {
+		c = str[i]
+		if (isalnum(c) != 0 || c == ' ')
+			dest[j++] = c;
+	}
+	dest[j] = '\0';
+}
+
+// put to-lower-case chars at pointer 'str' into array 'dest'
+void intolower(const char *str, char dest[]) {
+	int i;
+	char *strlow;
+	for (i = 0; i < strlen(str); i++) {
+		dest[i] = tolower(str[i]);
+	}
+	dest[i] = '\0';
+}
+
+#include <stdlib.h>
+
+// compare s1 and s1 numerically
+int numcmp(char *s1, char *s2) {
+	double v1, v2;
+
+	v1 = atof(s1);
+	v2 = atof(s2);
+	if (v1 < v2)
+		return -1;
+	else if (v1 > v2)
+		return 1;
+	else
+		return 0;
+}
+
+void swap(void *v[], int i, int j) {
+	void *temp;
+
+	temp = v[i];
+	v[i] = v[j];
+	v[j] = temp;
+}
+
+/********************************************************************************
+Functions for line reading and writing used in all versions
+from pages 108-110, in setup for 5-7
+*/
+
+#define MAXLEN 1000 	// max length of any input line
+int getaline(char *, int);
+char *alloc(int);
+
+// read input lines: collect and save chars of each line,
+//     put pointers to the lines in array for use by rest of program, and return count of input lines
+// it modifies char *lineptr[], which is used elsewhere in program
+int readlines(char *lineptr[], int maxlines) {
+	int len, nlines;
+	char *p, line[MAXLEN];
+
+	nlines = 0;
+	// loop and get lines until terminal input is length 0
+	while ((len = getaline(line, MAXLEN)) > 0) {
+		// if (nlines >= maxlines || (p = alloc(len)) == NULL) {
+		if (nlines >= maxlines) {
+			return -1;
+		}
+		p = alloc(len); // if len fits in array, allocp iterates by len, and returns the OLD allocp value 
+		if (p == 0) {  	// alloc returns 0 if value did not fit
+			return -1;
+		}
+		else {
+			line[len-1] = '\0';  // delete newline
+			// strcpy library function p 249: copy 'line' (type: const char *) into 'p' (type: char *)
+			// 'p' gets contents of 'line'
+			strcpy(p, line);  // put line into next empty value of array
+			lineptr[nlines++] = p; // put pointer to line into variable used throughout program
+		}
+	}
+	return nlines;
+}
+
+// write output lines in order they appear in 'lineptr',
+//     where they were already put in sorted order
+void writelines(char *lineptr[], int nlines) {
+	printf("\nLines after sorting:\n");
+	while (nlines-- > 0)
+		printf("%s\n", *lineptr++); // print char array pointed to by next element in lineptr
+}
+
+// page 101
+#define ALLOCSIZE 10000    // size of available space
+
+static char allocbuf[ALLOCSIZE];  // storage for alloc
+static char *allocp = allocbuf;  // next free position
+
+char *alloc(int n) {    // return pointer to n characters
+	if (allocbuf + ALLOCSIZE - allocp >= n) {  // it fits
+		allocp += n;
+		return allocp - n;  // old p
+	} else {
+		return 0;
+	}
+}
+
+// page 29
+int getaline(char s[], int lim) {
+	int c, i;
+
+	for (i=0; i<lim-1 && (c=getchar()) != EOF && c != '\n'; ++i)
+		s[i] = c;
+	if (c == '\n') {
+		s[i] = c;
+		++i;
+	}
+	s[i] = '\0';
+	return i;
+}
